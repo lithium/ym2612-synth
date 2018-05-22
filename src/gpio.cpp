dd3d0afce5f58c21118622adb001b36921c5d05b
lithium <code@hlidskialf.com>
2018-05-22 14:51:32 -0700

    updated analog button ranges when amplifier caps are in place

--- src/gpio.cpp
+++ src/gpio.cpp
@@ -32,10 +32,10 @@ static uint16_t _gpio_last = 0;
 // Button buttons[BUTTON_COUNT];
 
 struct { int min; int max; } _analog_button_ranges[] = {
-    {0,300},
-    {2800,3300},
-    {800,1200},
-    {2000,2500},
+    {0,600},
+    {3000,3300},
+    {800,1300},
+    {2200,2500},
 };
 AnalogButton analog_buttons[ANALOG_BUTTON_COUNT];
 
